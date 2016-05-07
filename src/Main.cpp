d08abbef667ded853f00322f74124a77e82580ea
Vahen <naeos@laposte.net>
2016-05-07 14:21:19 +0200

    [UPDATE] Main Mise en commentaire

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <iostream>
-#include <RayTracer.hpp>
 
 void usage() {
     std::cout << "Format : synthese_image -n [level to load] -i [input file] -o [output file]" << std::endl;
@@ -14,53 +13,53 @@ void usage() {
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 7) {
-        usage();
-        return 0;
-    }
-    std::string optionN(argv[1]);
-
-    if (optionN.compare("-n") != 0) {
-        usage();
-        return 0;
-    }
-    std::string valueOfLevel(argv[2]);
-    int value = std::stoi(valueOfLevel);
-
-
-    std::string optionI(argv[3]);
-    if (optionI.compare("-i") != 0) {
-        usage();
-        return 0;
-    }
-    std::string inputFile(argv[4]);
-
-    std::string optionO(argv[5]);
-    if (optionI.compare("-o") != 0) {
-        usage();
-        return 0;
-    }
-
-    std::string outputFile(argv[6]);
-
-    // TODO add parametre
-    switch (value) {
-        case 1:
-            RayTracer::level_1();
-            break;
-        case 2:
-            RayTracer::level_2();
-            break;
-        case 3:
-            RayTracer::level_3();
-            break;
-        case 4:
-            RayTracer::level_4();
-            break;
-    }
-
-
-    Parser parser("./listObjet");
+//    if (argc != 7) {
+//        usage();
+//        return 0;
+//    }
+//    std::string optionN(argv[1]);
+//
+//    if (optionN.compare("-n") != 0) {
+//        usage();
+//        return 0;
+//    }
+//    std::string valueOfLevel(argv[2]);
+//    int value = std::stoi(valueOfLevel);
+//
+//
+//    std::string optionI(argv[3]);
+//    if (optionI.compare("-i") != 0) {
+//        usage();
+//        return 0;
+//    }
+//    std::string inputFile(argv[4]);
+//
+//    std::string optionO(argv[5]);
+//    if (optionI.compare("-o") != 0) {
+//        usage();
+//        return 0;
+//    }
+//
+//    std::string outputFile(argv[6]);
+//
+//    // TODO add parametre
+//    switch (value) {
+//        case 1:
+//            RayTracer::level_1();
+//            break;
+//        case 2:
+//            RayTracer::level_2();
+//            break;
+//        case 3:
+//            RayTracer::level_3();
+//            break;
+//        case 4:
+//            RayTracer::level_4();
+//            break;
+//    }
+//
+//
+//    Parser parser("./listObjet");
 
 //    parser.parse();
 
