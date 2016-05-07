4afde74f0bb8a706f3cf997413b23cba7e2ba566
Vahen <naeos@laposte.net>
2016-05-07 22:26:10 +0200

    [UPDATE] RayTracer Ajout du lancer de rayon basique Il faudra ajouter la Camera plus tard

--- include/RayTracer.hpp
+++ include/RayTracer.hpp
@@ -6,15 +6,46 @@
 #define PROJECT_RAYTRACER_HPP
 
 #include "Scene.hpp"
-
+#include <Gui.hpp>
 class RayTracer {
 private:
-    Scene scene;
+    // Ajouter dimension
+    Scene m_scene;
+//    Camera camera;
+
+    // Concerne celui qui regarde
+    // A remplacer par la camera après
+    Point m_position; // Position de la camera
+    Vector m_orientation; // Orientation de la camera
+    float m_width, m_height;
+    float m_depth;
+    ///////////////////////
+
+    /// Resolution ///
+    float m_pixelWidth;
+    float m_pixelHeight;
+
+    /// Precalcule afin d'optimiser ///
+    float m_precompWidth;
+    float m_precompHeight;
+
+    Gui m_gui;
+
+    void updatePrecomp();
+
 public:
-    static void level_1();
-    static void level_2();
-    static void level_3();
-    static void level_4();
+    RayTracer();
+
+    RayTracer(float width, float heigth, float depth, float pixelWidth, float pixelHeight, Scene scene,Gui gui);
+
+    ~RayTracer();
+
+    void draw();
+
+    void computColor(Ray const& ray, Vector color);
+
+    void updateCamera(float width, float height, Point const& origin, Vector const& direction);
+
 
 };
 
--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -2,4 +2,65 @@
 // Created by Narex on 05/05/2016.
 //
 
+#include <Shape.hpp>
+
 #include "RayTracer.hpp"
+
+RayTracer::RayTracer() { }
+
+RayTracer::RayTracer(float width, float height, float depth, float pixelWidth, float pixelHeight, Scene scene, Gui gui)
+        : m_width(width),
+          m_height(height),
+          m_depth(depth),
+          m_pixelWidth(pixelWidth),
+          m_pixelHeight(pixelHeight),
+          m_scene(scene),
+          m_gui(gui) {
+}
+
+RayTracer::~RayTracer() { }
+
+void RayTracer::draw() {
+
+    for (int i = 0; i < m_pixelWidth; ++i) {
+        for (int j = 0; j < m_pixelHeight; ++j) {
+            Vector directionTempo(
+                    m_precompWidth * (i - m_pixelWidth / 2.f),
+                    m_precompHeight * (j - m_pixelHeight / 2.f),
+                    m_depth
+            );
+            Ray ray(m_position, directionTempo);
+            Vector color(0, 0, 0);
+            // Calcul de la couleur a afficher
+            computColor(ray, color);
+
+            m_gui.setPixel(i, j, color);
+        }
+    }
+
+}
+
+void RayTracer::updatePrecomp() {
+    m_precompWidth = m_width / m_pixelWidth;
+    m_precompHeight = m_height / m_pixelHeight;
+}
+
+void RayTracer::computColor(Ray const& ray, Vector color) {
+    float dist;
+    float index = m_scene.getFirstCollision(ray, dist);
+    if (index < 0)
+        return;
+
+    Vector normal;
+    Shape shape = m_scene.getShape(index);
+    shape.computeColorNormal(ray, dist, color, normal);
+}
+
+// TODO A changer l'avenir
+void RayTracer::updateCamera(float width, float height, Point const& position, Vector const& orientation) {
+    this->m_width = width;
+    this->m_height = height;
+    this->m_position = position;
+    this->m_orientation = orientation;
+    updatePrecomp();
+}
