373716689feb7a9419e8052ce98f65d8af8838d0
Thomas Kieffer <thomaskieffer01@gmail.com>
2016-05-14 19:28:48 +0200

    [ADD] PPMExporter.

---
+++ src/PPMExporter.cpp
@@ -0,0 +1,21 @@
+#include "PPMExporter.hpp"
+#include <iostream>
+
+PPMExporter::PPMExporter(std::string const& path, int width, int height)
+: 	m_stream(path)
+{
+	if (m_stream.fail()) {
+		std::cerr << "Cannot open file named: " << path << std::endl;
+	}
+	m_stream << "P3" << std::endl << width << " " << height << "255" << std::endl; 
+}
+
+PPMExporter::~PPMExporter() {
+	if (m_stream.is_open()) {
+		m_stream.close();
+	}
+}
+
+void PPMExporter::writePixel(Vector const& color) {
+	m_stream << color.x() << " " << color.y() << " " << color.z() << "	    ";
+}
